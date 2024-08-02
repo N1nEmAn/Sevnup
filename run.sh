23939a75d9397dd5c503ba4c4c1912b5d65f0cef
N1nEmAn <1332037591@qq.com>
2024-08-02 17:10:03 +0800

    updat

--- run.sh
+++ run.sh
@@ -48,10 +48,10 @@ if [ ! -d "$squashfs_root_path" ]; then
   echo -e "\033[0;31m[x]\033[0m The provided path is not a directory."
   print_help
 fi
-sudo chmod 777 "$squashfs_root_path/lib/*"
-sudo chmod 777 "$squashfs_root_path/bin/*"
-sudo chmod 777 "$squashfs_root_path/usr/lib/*"
-sudo chmod 777 "$squashfs_root_path/usr/bin/*"
+sudo chmod 777 $squashfs_root_path/lib/*
+sudo chmod 777 $squashfs_root_path/bin/*
+sudo chmod 777 $squashfs_root_path/usr/lib/*
+sudo chmod 777 $squashfs_root_path/usr/bin/*
 # Initial log file
 mkdir -p log
 qemu_log="./log/qemu.log"
