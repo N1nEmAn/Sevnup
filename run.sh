3810ed366a890e5c3ed9663e2b471d8e5d8a9165
Wanli Chen <118088443+N1nEmAn@users.noreply.github.com>
2024-10-27 20:37:20 +0800

    Update run.sh for armhf and nograph

--- run.sh
+++ run.sh
@@ -18,8 +18,9 @@ print_help() {
   echo "Usage: $0 [ARCHITECTURE] [ROOT_PATH]"
   echo
   echo "ARCHITECTURE:"
-  echo "  mipsel  - MIPS architecture"
-  echo "  armel   - ARM architecture"
+  echo "  mipsel   - MIPS architecture"
+  echo "  armel    - ARM architecture"
+  echo "  armhf   - ARM architecture for vexpress"
   echo
   echo "ROOT_PATH:"
   echo "  The path to the root directory of the squashfs-root."
@@ -37,7 +38,7 @@ fi
 
 # Check for architecture argument
 arch="$1"
-if [ "$arch" != "mipsel" ] && [ "$arch" != "armel" ]; then
+if [ "$arch" != "mipsel" ] && [ "$arch" != "armel" ] && [ "$arch" != "armhf" ]; then
   echo -e "\033[0;31m[x]\033[0m Unsupported architecture: $arch"
   print_help
 fi
@@ -49,9 +50,11 @@ if [ ! -d "$squashfs_root_path" ]; then
   print_help
 fi
 sudo chmod 777 $squashfs_root_path/lib/*
-sudo chmod 777 $squashfs_root_path/bin/*
-sudo chmod 777 $squashfs_root_path/usr/lib/*
-sudo chmod 777 $squashfs_root_path/usr/bin/*
+sudo chmod 777 $squashfs_root_path/bin/* 2 &>1
+sudo chmod 777 $squashfs_root_path/sbin/* 2 &>1
+sudo chmod 777 $squashfs_root_path/usr/lib/* 2 &>1
+sudo chmod 777 $squashfs_root_path/usr/bin/* 2 &>1
+sudo chmod 777 $squashfs_root_path/usr/sbin/* 2 &>1
 # Initial log file
 mkdir -p log
 qemu_log="./log/qemu.log"
@@ -64,8 +67,18 @@ touch ./log/http.log
 echo "                                                    "
 echo "===============check image====================="
 echo "                                                    "
+if [ "$arch" == "armhf" ]; then
+  # Check and download ARMHF files
+  if [ ! -f "./img/debian_wheezy_armhf_standard.qcow2" ] || [ ! -f "./img/vmlinuz-3.2.0-4-vexpress" ] || [ ! -f "./img/initrd.img-3.2.0-4-vexpress" ]; then
+    echo -e "\033[0;33m[!]\033[0m Downloading required ARMHF files..."
+    wget -P ./img https://people.debian.org/~aurel32/qemu/armhf/debian_wheezy_armhf_standard.qcow2
+    wget -P ./img https://people.debian.org/~aurel32/qemu/armhf/vmlinuz-3.2.0-4-vexpress
+    wget -P ./img https://people.debian.org/~aurel32/qemu/armhf/initrd.img-3.2.0-4-vexpress
+    qemu-img resize ./img/debian_wheezy_armhf_standard.qcow2 32G
+  fi
 
-if [ "$arch" == "armel" ]; then
+elif [ "$arch" == "armel" ]; then
+  # Check and download ARMEL files
   if [ -f ./img/vmlinuz-3.2.0-4-versatile ]; then
     echo -e "\033[0;32m[+]\033[0m vmlinuz-3.2.0-4-versatile exists."
   else
@@ -82,13 +95,15 @@ if [ "$arch" == "armel" ]; then
     wget -P ./img https://people.debian.org/~aurel32/qemu/armel/debian_wheezy_armel_standard.qcow2
     echo "[o] debian_wheezy_armel_standard.qcow2 is downloading..."
   fi
+
 else
+  # Handle other architectures (e.g., mipsel)
   if [ -f ./img/vmlinux-3.2.0-4-4kc-malta ]; then
     echo -e "\033[0;32m[+]\033[0m vmlinux-3.2.0-4-4kc-malta exists."
   else
     echo -e "\033[0;31m[x]\033[0m vmlinux-3.2.0-4-4kc-malta is missing. Downloading..."
     wget -P ./img https://people.debian.org/~aurel32/qemu/mipsel/vmlinux-3.2.0-4-4kc-malta
-    echo "[o] vmlinux-2.6.32-5-4kc-malta is downloading..."
+    echo "[o] vmlinux-3.2.0-4-4kc-malta is downloading..."
   fi
 
   if [ -f ./img/debian_wheezy_mipsel_standard.qcow2 ]; then
@@ -99,6 +114,7 @@ else
     echo "[o] debian_wheezy_mipsel_standard.qcow2 is downloading..."
   fi
 fi
+
 echo "                                                    "
 
 # Extract the last directory name from the root path
@@ -136,8 +152,8 @@ echo -e "[o] creating /etc/qemu-ifup... and backup existing /etc/qemu-ifup to /e
 cat <<'EOF' | sudo tee /etc/qemu-ifup >/dev/null
 #!/bin/sh
 
-ip addr show
-ip link show
+# ip addr show
+# ip link show
 cat /etc/qemu-ifup.bak
 
 EOF
@@ -147,18 +163,31 @@ sudo chmod +x /etc/qemu-ifup
 # Run qemu-system with nohup and redirect output to log/output.log
 cp ./load_in_mips.sh "$squashfs_root_path/../"
 cp ./bridge.sh "$squashfs_root_path/../"
-
-if [ "$arch" == "armel" ]; then
-  sudo nohup qemu-system-arm -M versatilepb -kernel ./img/vmlinuz-3.2.0-4-versatile -initrd ./img/initrd.img-3.2.0-4-versatile -hda ./img/debian_wheezy_armel_standard.qcow2 -append "root=/dev/sda1 console=tty0" -net nic -net tap,ifname=tap0 -s >"$qemu_log" 2>&1 &
-  echo -e "\033[0;32m[+]\033[0m now qemu-system-${arch} is running!"
-  echo -e "[o] in ${arch}, use username 'root' and password 'root' to login."
-else
-  sudo nohup qemu-system-mipsel -M malta -kernel ./img/vmlinux-3.2.0-4-4kc-malta -hda ./img/debian_wheezy_mipsel_standard.qcow2 -append "root=/dev/sda1 console=tty0" -net nic -net tap,ifname=tap0 -s >"$qemu_log" 2>&1 &
-  echo -e "\033[0;32m[+]\033[0m now qemu-system-${arch} is running!"
-  echo -e "[o] in ${arch}, use username 'root' and password 'root' to login."
-fi
+echo -e "\033[0;32m[+]\033[0m now qemu-system-${arch} is running!"
+echo -e "[o] in ${arch}, use username 'root' and password 'root' to login."
 echo -e "[o] input:'ifconfig eth0 10.10.10.2/24' to config ip address."
 echo -e "[o] input:'ping 10.10.10.1' to test if the connection is successful."
 echo -e "[o] input:'wget http://10.10.10.1:8000/load_in_mips.sh' to download script."
 echo -e "[o] input:'sh load_in_mips.sh' to finish all the work."
 echo "                                                    "
+if [ "$arch" == "armel" ]; then
+  sudo qemu-system-arm -M versatilepb -nographic \
+    -kernel ./img/vmlinuz-3.2.0-4-versatile \
+    -initrd ./img/initrd.img-3.2.0-4-versatile \
+    -hda ./img/debian_wheezy_armel_standard.qcow2 \
+    -append "root=/dev/sda1 console=tty0" -net nic -net tap,ifname=tap0 -s
+
+elif [ "$arch" == "armhf" ]; then
+  # 启动 qemu-system-arm
+  sudo qemu-system-arm -M vexpress-a9 -nographic \
+    -kernel ./img/vmlinuz-3.2.0-4-vexpress \
+    -initrd ./img/initrd.img-3.2.0-4-vexpress \
+    -drive if=sd,file=./img/debian_wheezy_armhf_standard.qcow2 \
+    -append "root=/dev/mmcblk0p2 console=tty0" -net nic -net tap,ifname=tap0 -s
+else
+  sudo nohup qemu-system-mipsel -M malta \
+    -kernel ./img/vmlinux-3.2.0-4-4kc-malta \
+    -hda ./img/debian_wheezy_mipsel_standard.qcow2 \
+    -append "root=/dev/sda1 console=tty0" -net nic -net tap,ifname=tap0 -s \
+    >"$qemu_log" 2>&1 &
+fi
